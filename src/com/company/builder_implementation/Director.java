1212fe1fae5c3f6f00f994c2691f9a24d1aae793
drakeaifoce <drakeaifoce@gmail.com>
2021-03-15 00:02:23 +0600

    q

--- src/com/company/builder_implementation/Director.java
+++ src/com/company/builder_implementation/Director.java
@@ -6,6 +6,13 @@ public class Director {
 
     public void createHouse(IHouseBuilder houseBuilder) {
         houseBuilder.reset();
+        houseBuilder.buildWalls(4);
+        houseBuilder.buildWindows(4);
         houseBuilder.buildDoors(4);
+        houseBuilder.buildGarage(1);
+        houseBuilder.buildFancyStatues(2);
+        houseBuilder.buildSwimmingPool(1);
+        houseBuilder.buildGarden(1);
+        houseBuilder.buildRoof(1);
     }
 }
